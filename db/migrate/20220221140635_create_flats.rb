a045f59e7abbc9f6c7f1adfc41a2425ea29de1e0
Lucas Virgil <virgill@email.unc.edu>
2022-02-21 15:53:13 +0100

    user model with devise setup

--- db/migrate/20220221140635_create_flats.rb
+++ db/migrate/20220221140635_create_flats.rb
@@ -1,10 +1,10 @@
 class CreateFlats < ActiveRecord::Migration[6.1]
   def change
     create_table :flats do |t|
-      t.string :title,
-      t.string :address,
-      t.string :dates,
-      t.integer, :price
+      t.string :title
+      t.string :address
+      t.string :dates
+      t.integer :price
       t.string :description
 
       t.timestamps
