0b081b4254fb3d59fd9f464c2915a805c838e86d
Philippe De Andria <philippedeandria12@hotmail.com>
2022-02-23 12:14:08 +0100

    Fully functional submit button on Home page

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,7 @@
 class Flat < ApplicationRecord
   # reference to the user model. user_id and user (id)
+  include PgSearch::Model
+  pg_search_scope :search_by_location, against: :address, using: { tsearch: {
+    prefix: true
+  } }
 end
