f9a336060ca79021eadc8585e69cc440fbdcef6a
Philippe De Andria <philippedeandria12@hotmail.com>
2022-02-24 12:36:24 +0100

    bookings controller and booking router

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,39 @@
+class BookingsController < ApplicationController
+  def new
+    @flat = Flat.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def create
+    @flat = flat.find(params[:id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.flat = @flat
+    if @booking.save
+      redirect_to flat_path(@flat.id)
+    else
+      render :new
+    end
+  end
+
+  def change_status
+    @booking = Booking.find(params[:id])
+    if params[:status].present?
+      @booking.update(status: params[:status])
+    end
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id]) # not sure
+    @flat = @booking.flat
+    @booking.destroy
+    redirect_to flat_path(@flat) # not sure about the path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :flats
+  resources :flats do
+    resources :bookings, only: %i[new, create]
+  end
   resources :users, only: :show
+  resources :booking, only: :destroy do
+    member do
+      patch :change_status
+    end
+  end
 end
