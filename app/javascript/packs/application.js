6ee9c24135744e278d0d5319bda4aa40f10f78f4
Lucas Virgil <virgill@email.unc.edu>
2022-05-05 19:01:35 -0400

    new navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import  "../application.css"
 import { initMapbox } from "./init_mapbox"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 import "controllers"
-import "bootstrap"
 document.addEventListener("turbolinks:load", () => {
 
   initMapbox();
